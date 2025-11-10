b0a31040680c4c85975c7a7fec9e30bf06dd31e9
ekber_ekber <ekberehmedov154@gmail.com>
2025-11-10 14:57:03 +0400

    Update snippet_helper.cpp

--- snippet_helper.cpp
+++ snippet_helper.cpp
@@ -1,6 +1,6 @@
 /*
-    Run this code here: https://www.onlinegdb.com/online_c++_compiler
-    And select "Text" input.
+    Run this code in your IDE with using "input.txt".
+    Or you can run it on here: "https://www.onlinegdb.com/online_c++_compiler". But you need to select text input type and delete freopens(20 and 21-th lines).
 */
 #include <bits/stdc++.h>
 #define GOOD_LUCK ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
@@ -17,6 +17,8 @@ constexpr int MAX = 2e+5 + 1, INF = 2e+16, MOD = 1e+9 + 7, K = 31;
 signed main() {
 
     GOOD_LUCK
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
     string a;
     vector <string> x;
@@ -24,8 +26,13 @@ signed main() {
         string b = "";
         b += '"';
         int cnt=0;
-        for (int i=0; i < a.size(); i++) {
-            if (a[i] == ' ') {
+        for (char &i : a) {
+            if (i == 9) {
+                // while (cnt--) b = b + ' '; 
+                b += "\\";
+                b += 't';
+            }
+            else if (i == ' ') {
                 cnt++;
                 if (cnt == 4) {
                     b += "\\";
@@ -38,7 +45,7 @@ signed main() {
                     for (int j=0; j < cnt; j++) b += ' ';
                 }
                 cnt = 0;
-                b += a[i];
+                b += i;
             }
         }
         b += '"';
