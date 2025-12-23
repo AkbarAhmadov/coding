c1cb25442e6c4eeb37dd3c1b6a657cf8143ecf24
ekber_ekber <ekberehmedov154@gmail.com>
2025-12-23 19:49:01 +0400

    Update snippet_helper.cpp

--- snippet_helper.cpp
+++ snippet_helper.cpp
@@ -1,6 +1,8 @@
 /*
     Run this code in your IDE with using "input.txt".
-    Or you can run it on here: "https://www.onlinegdb.com/online_c++_compiler". But you need to select text input type and delete freopens(20 and 21-th lines).
+    Or you can run it on here: "https://www.onlinegdb.com/online_c++_compiler". But you need to select text input type and delete freopens(24 and 25-th lines).
+    
+    Also you must change the name, prefix and description in 19-th line. Remove ???-s and write whatever you want.
 */
 #include <bits/stdc++.h>
 #define GOOD_LUCK ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
@@ -14,11 +16,17 @@ using namespace std;
 
 constexpr int MAX = 2e+5 + 1, INF = 2e+16, MOD = 1e+9 + 7, K = 31;
 
+string NAME = "???", PREFIX = "???", DESC = "???";
+
 signed main() {
 
     GOOD_LUCK
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    cout << '"' << NAME << '"' << ": {\n";
+    cout << "\t" << '"' << "prefix" << '"' << ": " << '"' << PREFIX << '"' << ",\n";
+    cout << "\t" << '"' << "body" << '"' << ": [\n";
 
     string a;
     vector <string> x;
@@ -28,7 +36,6 @@ signed main() {
         int cnt=0;
         for (char &i : a) {
             if (i == 9) {
-                // while (cnt--) b = b + ' '; 
                 b += "\\";
                 b += 't';
             }
@@ -50,6 +57,9 @@ signed main() {
         }
         b += '"';
         b += ',';
-        cout << b << endl;
+        cout << "\t\t" << b << endl;
     }
+    cout << "\t],\n";
+    cout << "\t" << '"' << "description" << '"' << ": " << '"' << DESC << '"' << ",\n";
+    cout << "},";
 }
